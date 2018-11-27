896300fef081a57dcde9677b72690f44a9247f4c
baerge <43047842+baerge@users.noreply.github.com>
2018-11-27 17:33:40 -0500

    Update main.js
    updating main js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const updatePrice = (ev) => {
         e.target.classList.add('price');
         console.log(e.target.value);
         console.log(ev.target.getAttribute('data-price'));
-    }, 25, ev);
+    }, 10, ev);
 }
 let min = document.getElementById('min');
 let max = document.getElementById('max');
